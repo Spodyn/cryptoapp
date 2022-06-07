2d9abef43806340c9ea6e532131b28833f7e25e8
Spodyn <80603563+Spodyn@users.noreply.github.com>
2022-06-07 22:50:41 +0200

    navbarAndFooter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App'
+
+import App from './App';
+
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
-<Router>
-    <App />
-</Router>
-, document.getElementById('root'));
+  <React.StrictMode>
+    <Router>
+        <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
